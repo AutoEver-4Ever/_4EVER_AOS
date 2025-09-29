cc00c2a1a6ed0177472d40071e33e6f97e6a8666
parkjiwon <ji11won@naver.com>
2025-09-29 17:14:22 +0900

    chore: 의존성 설정
    - agp : "9.0.0-alpha05" -> 8.13.0
    - kotlin : 2.0.21 -> 2.2.20
    - JVM : 11 -> 17
    - androidx.viewmodel, navigation, retrofit, kotlinx-serialization, kotlinx-coroutine, coil3, hilt, timber 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android) // AGP 8.x에서는 필요
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization) // Kotlinx Serialization
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -29,28 +33,70 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
     }
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
+    // Core Android dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx) // ViewModel
+    implementation(libs.androidx.navigation.compose) // Navigation
+
+    // Jetpack Compose dependencies
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    // Debug dependencies
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // Testing dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Retrofit dependencies
+    implementation(libs.retrofit)
+    implementation(libs.logging.interceptor)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+    // Coroutines dependencies
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    // Coil dependency for image loading
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+    // Hilt dependencies
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    // Timber dependency for logging
+    implementation(libs.timber)
+//    // DataStore dependencies
+//    implementation("androidx.datastore:datastore-preferences:1.0.0")
+//    // Room dependencies
+//    implementation("androidx.room:room-runtime:2.5.2")
+//    kapt("androidx.room:room-compiler:2.5.2")
+//    implementation("androidx.room:room-ktx:2.5.2")
+//    // Security dependency
+//    implementation("androidx.security:security-crypto:1.1.0-alpha03")
+//    // Gson dependency for JSON parsing
+//    implementation("com.google.code.gson:gson:2.10.1")
+//    // Splash Screen dependency
+//    implementation("androidx.core:core-splashscreen:1.0.1")
+//    // ViewBinding Property Delegate dependency
+//    implementation("com.github.Zhuinden:viewbindingpropertydelegate-kt:1.5.6")
 }
