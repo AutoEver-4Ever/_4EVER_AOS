d9dbb5786d8e53095f8640fa90a9d87705747e82
parkjiwon <ji11won@naver.com>
2025-09-29 18:48:21 +0900

    test: ktlint 적용 테스트

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.ktlint)
 }
