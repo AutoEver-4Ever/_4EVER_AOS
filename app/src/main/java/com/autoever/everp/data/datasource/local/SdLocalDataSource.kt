e4a0247f76af8644a0a0dd8fd31aff053a46c913
parkjiwon <ji11won@naver.com>
2025-10-20 03:44:23 +0900

    feat(sd): DataSource interface 생성

---
+++ app/src/main/java/com/autoever/everp/data/datasource/local/SdLocalDataSource.kt
@@ -0,0 +1,3 @@
+package com.autoever.everp.data.datasource.local
+
+interface SdLocalDataSource
---
+++ app/src/main/java/com/autoever/everp/data/datasource/remote/SdRemoteDataSource.kt
@@ -0,0 +1,22 @@
+package com.autoever.everp.data.datasource.remote
+
+import com.autoever.everp.domain.model.Quotation
+import com.autoever.everp.domain.model.QuotationDetail
+import com.autoever.everp.domain.model.QuotationListItem
+import kotlinx.coroutines.flow.Flow
+
+interface SdRemoteDataSource {
+    fun fetchQuotationList(): Flow<List<QuotationListItem>>
+
+    fun getQuotationList(): Flow<List<QuotationListItem>>
+
+    fun getQuotationDetail(quotationId: String): Flow<QuotationDetail?>
+
+    suspend fun createQuotation(quotation: Quotation): Boolean
+
+    suspend fun updateQuotation(quotation: Quotation): Boolean
+
+    suspend fun deleteQuotation(quotationId: Long): Boolean
+
+    suspend fun requestQuotationApproval(quotationId: Long): Boolean
+}
