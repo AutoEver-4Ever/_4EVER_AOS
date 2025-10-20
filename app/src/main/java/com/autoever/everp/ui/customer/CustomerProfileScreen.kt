3223fc2062ea9fc4e605fbfd3e2dd286e827b0d8
parkjiwon <ji11won@naver.com>
2025-10-20 18:05:33 +0900

    feat(ui): 고객사 기초 화면 생성

---
+++ app/src/main/java/com/autoever/everp/ui/customer/CustomerProfileScreen.kt
@@ -0,0 +1,10 @@
+package com.autoever.everp.ui.customer
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerProfileScreen() {
+    // 고객용 홈 화면 UI 구현
+    Text("Customer Profile Screen")
+}
---
+++ app/src/main/java/com/autoever/everp/ui/customer/CustomerVoucherScreen.kt
@@ -0,0 +1,10 @@
+package com.autoever.everp.ui.customer
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomerVoucherScreen() {
+    // 고객용 바우처 화면 UI 구현
+    Text("Customer Voucher Screen")
+}
