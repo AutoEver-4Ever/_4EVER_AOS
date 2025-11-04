9ceef167bd19eddaf60b76eb21faabe17fd3815b
parkjiwon <ji11won@naver.com>
2025-11-04 12:08:01 +0900

    feat(model): 공급사 관련 domain-model 정의
    - SupplierCategoryEnum : 공급사가 공급하는 물품의 타입
    - SupplierStatusEnum : 공급사 상태 타입

---
+++ app/src/main/java/com/autoever/everp/domain/model/supplier/SupplierCatetoryEnum.kt
@@ -0,0 +1,8 @@
+package com.autoever.everp.domain.model.supplier
+
+enum class SupplierCatetoryEnum {
+    UNKKNOWN, // 알 수 없음, 기본값
+    MATERIAL, // 자재
+    ITEMS, // 물품
+    ;
+}
---
+++ app/src/main/java/com/autoever/everp/domain/model/supplier/SupplierStatusEnum.kt
@@ -0,0 +1,7 @@
+package com.autoever.everp.domain.model.supplier
+
+enum class SupplierStatusEnum {
+    ACTIVE, // 활성
+    INACTIVE, // 비활성
+    ;
+}
