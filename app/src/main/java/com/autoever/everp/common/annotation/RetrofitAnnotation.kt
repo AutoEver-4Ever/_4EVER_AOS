f8de6a7ed2a0b6792b83769704bdb45d3e63a9c0
parkjiwon <ji11won@naver.com>
2025-11-05 22:43:09 +0900

    feat(auth): AuthApi용 Retrofit 객체 생성
    - 아직 미완성 Auth를 위한 정확한 스펙이 필요하다

---
+++ app/src/main/java/com/autoever/everp/common/annotation/RetrofitAnnotation.kt
@@ -0,0 +1,11 @@
+package com.autoever.everp.common.annotation
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AuthRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class NormalRetrofit
--- app/src/main/java/com/autoever/everp/di/NetworkModule.kt
+++ app/src/main/java/com/autoever/everp/di/NetworkModule.kt
@@ -1,8 +1,11 @@
 package com.autoever.everp.di
 
 import com.autoever.everp.BuildConfig
+import com.autoever.everp.common.annotation.AuthRetrofit
+import com.autoever.everp.common.annotation.NormalRetrofit
 import com.autoever.everp.data.datasource.remote.http.service.AlarmApi
 import com.autoever.everp.data.datasource.remote.http.service.AlarmTokenApi
+import com.autoever.everp.data.datasource.remote.http.service.AuthApi
 import com.autoever.everp.data.datasource.remote.http.service.FcmApi
 import com.autoever.everp.data.datasource.remote.http.service.HrmApi
 import com.autoever.everp.data.datasource.remote.http.service.ImApi
@@ -71,6 +74,32 @@ object NetworkModule {
 
     @Provides
     @Singleton
+    @AuthRetrofit
+    fun provideAuthRetrofit(
+        json: Json,
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): Retrofit {
+        // 인증 전용 Retrofit (Auth 서버용)
+        // TODO : 공통 OkHttpClient 사용하도록 변경 검토
+        // TODO : 미완성 - 타임아웃 등 설정 추가 필요
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .apply {
+                connectTimeout(30, TimeUnit.SECONDS)
+                readTimeout(30, TimeUnit.SECONDS)
+                writeTimeout(30, TimeUnit.SECONDS)
+            }.build()
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.AUTH_BASE_URL)
+            .client(client)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @NormalRetrofit
     fun provideRetrofit(
         okHttpClient: OkHttpClient,
         json: Json,
@@ -89,41 +118,46 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun provideAlarmApiService(retrofit: Retrofit): AlarmApi =
+    fun provideAlarmApiService(@NormalRetrofit retrofit: Retrofit): AlarmApi =
         retrofit.create(AlarmApi::class.java)
 
     @Provides
     @Singleton
-    fun provideFcmTokenApiService(retrofit: Retrofit): AlarmTokenApi =
+    fun provideFcmTokenApiService(@NormalRetrofit retrofit: Retrofit): AlarmTokenApi =
         retrofit.create(AlarmTokenApi::class.java)
 
     @Provides
     @Singleton
-    fun provideSdApiService(retrofit: Retrofit): SdApi =
+    fun provideSdApiService(@NormalRetrofit retrofit: Retrofit): SdApi =
         retrofit.create(SdApi::class.java)
 
     @Provides
     @Singleton
-    fun provideHrmApiService(retrofit: Retrofit): HrmApi =
+    fun provideHrmApiService(@NormalRetrofit retrofit: Retrofit): HrmApi =
         retrofit.create(HrmApi::class.java)
 
     @Provides
     @Singleton
-    fun provideFcmFinanceApiService(retrofit: Retrofit): FcmApi =
+    fun provideFcmFinanceApiService(@NormalRetrofit retrofit: Retrofit): FcmApi =
         retrofit.create(FcmApi::class.java)
 
     @Provides
     @Singleton
-    fun provideInventoryApiService(retrofit: Retrofit): ImApi =
+    fun provideInventoryApiService(@NormalRetrofit retrofit: Retrofit): ImApi =
         retrofit.create(ImApi::class.java)
 
     @Provides
     @Singleton
-    fun provideMaterialApiService(retrofit: Retrofit): MmApi =
+    fun provideMaterialApiService(@NormalRetrofit retrofit: Retrofit): MmApi =
         retrofit.create(MmApi::class.java)
 
     @Provides
     @Singleton
-    fun provideUserApiService(retrofit: Retrofit): UserApi =
+    fun provideUserApiService(@NormalRetrofit retrofit: Retrofit): UserApi =
         retrofit.create(UserApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideUserAuthApiService(@AuthRetrofit retrofit: Retrofit): AuthApi =
+        retrofit.create(AuthApi::class.java)
 }
