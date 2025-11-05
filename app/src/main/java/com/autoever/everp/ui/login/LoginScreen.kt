c5c9cef91a396a9939cbe6107feab00c400de67b
parkjiwon <ji11won@naver.com>
2025-11-05 17:48:50 +0900

    fix(): ktlint 수정용

--- app/src/main/java/com/autoever/everp/ui/login/LoginScreen.kt
+++ app/src/main/java/com/autoever/everp/ui/login/LoginScreen.kt
@@ -1,13 +1,12 @@
 package com.autoever.everp.ui.login
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -16,9 +15,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,13 +26,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
-import android.content.res.Configuration
-import com.autoever.everp.ui.theme.EverpTheme
-import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.ui.unit.dp
 import com.autoever.everp.R
-import com.autoever.everp.domain.model.user.UserTypeEnum
+import com.autoever.everp.ui.theme.EverpTheme
 
 @Composable
 fun LoginScreen(
@@ -57,7 +53,7 @@ fun LoginScreen(
                 .navigationBarsPadding()
         ) {
             // 로고: 중앙 상단
-            androidx.compose.foundation.Image(
+            Image(
                 painter = painterResource(id = R.drawable.everp_logo),
                 contentDescription = "EVERP 로고",
                 modifier = Modifier
